3b488c34506a7e4d01126786c6f1f8021425f315
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-07-19 12:11:42 +0530

    adding middleware and global cahtes

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
+// const express = require("express");
+// const app = express();
+
+// app.get("/health-checkup", function (req, res) {
+//   const username = req.headers.username;
+//   const password = req.headers.password;
+//   const kidneyId = parseInt(req.query.kidneyId);
+
+//   if (username != "harkirat" || password != "pass") {
+//     res.status(400).json({ msg: "somethings up with your inputs" });
+//     return;
+//   }
+
+//   if (kidneyId !== 1 && kidneyId !== 2) {
+//     res.status(400).json({ msg: "sssomethings up with your inputs" });
+//     return;
+//   }
+
+//   res.json({
+//     msg: "Your kidney is fine!",
+//   });
+// });
+
+// app.listen(3000);
+
+// Find the average time your server is taking to handle requests
+
 const express = require("express");
 const app = express();
 
-app.get("/health-checkup", function (req, res) {
-  const username = req.headers.username;
-  const password = req.headers.password;
-  const kidneyId = parseInt(req.query.kidneyId);
+app.use(express.json());
 
-  if (username != "harkirat" || password != "pass") {
-    res.status(400).json({ msg: "somethings up with your inputs" });
-    return;
-  }
+app.post("/health-checkup", function (req, res) {
+  const kidneys = req.body.kidneys;
+  const kidneyLength = kidneys.length;
 
-  if (kidneyId !== 1 && kidneyId !== 2) {
-    res.status(400).json({ msg: "sssomethings up with your inputs" });
-    return;
-  }
-
-  res.json({
-    msg: "Your kidney is fine!",
-  });
+  res.send("you have " + kidneyLength + "kidneys");
 });
-
 app.listen(3000);
